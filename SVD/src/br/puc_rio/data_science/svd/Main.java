0ec3acf8f9b0ec11cfd625ab268d77471b65dc55
luisaRosa <luisasilveirarosa@gmail.com>
2017-10-03 22:42:43 -0300

    Add files via upload

---
+++ SVD/src/br/puc_rio/data_science/svd/Main.java
@@ -0,0 +1,35 @@
+package br.puc_rio.data_science.svd;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Image_to_Matrix image_to_Matrix = new Image_to_Matrix();
+		BufferedImage image = null;
+		File arq = new File("images/image1.jpg");
+		try {
+			image = ImageIO.read(arq);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(image.getWidth()+" "+ image.getHeight());
+		int [][] A = image_to_Matrix.getMatrix(image);// matriz original da imagem
+		int[][] At = image_to_Matrix.getMatrixTransposed
+				(A, image.getWidth(), image.getHeight());// matriz transposta da imagem	
+		int [][] U = image_to_Matrix.multiply_matrices(A, At);
+		image_to_Matrix.print_matrix(U, image.getWidth(), image.getWidth());
+		
+		
+		
+		
+		
+	
+	
+
+}}
