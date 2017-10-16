37a21f9f33e8956587fcac582516945823183da5
luisaRosa <luisasilveirarosa@gmail.com>
2017-10-16 18:49:08 -0200

    Add files via upload

--- SVD/src/br/puc_rio/data_science/svd/ImageToMatrix.java
+++ SVD/src/br/puc_rio/data_science/svd/ImageToMatrix.java
@@ -1,5 +1,9 @@
 package br.puc_rio.data_science.svd;
+import java.awt.Color;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
 
 public class ImageToMatrix {
 	
@@ -8,13 +12,27 @@ public class ImageToMatrix {
 	    int image_height = image.getHeight();// Pega a altura da imagem
 		double[][] A = new double[image_width][image_height];
 		
+		
 		for(int x=0; x< image_width;x++) {//gera a matriz
-			for(int y=0; y< image_height;y++) 
-				A[x][y] = image.getRGB(x, y);			
+			for(int y=0; y< image_height;y++) {
+				A[x][y] = image.getRGB(x, y);	
+			     System.out.println(A[x][y]);}
 		}		
 		return A;
 	}
 	
+	public int[] matrixToVector(double[][] matrix, int w, int h) {
+		int[] vector = new int[w*h];
+		int d = 0;
+		for(int x=0; x<w;x++) {//gera a matriz
+			for(int y=0; y<h;y++) {
+				vector[d] = (int) matrix[x][y];
+				
+				d++;}
+			    
+			}
+		return vector;
+	}
 	public void printMatrix(double [][] matrix, int w, int h) {// imprime uma dada matriz
 		
 		for(int x=0; x< w;x++) {
@@ -23,4 +41,28 @@ public class ImageToMatrix {
 		System.out.println();
 		}		
 	}	
-}
+	
+	public BufferedImage getImage(double[] [] matrix, int w, int h) {
+		/*
+		final BufferedImage image =
+			    new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+			
+		           
+		            image.setRGB(0,0,w,h,matrix, 0, w);   */
+		
+		final BufferedImage image =  new BufferedImage(w, h,BufferedImage.TYPE_INT_ARGB);
+		WritableRaster raster = image.getRaster();
+		for (int i = 0; i < w; i++) {
+		for (int j = 0; j < h; j++) {
+		raster.setSample(i, j,image.getSampleModel().getDataType(), matrix[i][j]);
+		image.setData(raster);
+		
+		
+		}
+		}
+		
+		
+
+			
+		return image;
+	}}
--- SVD/src/br/puc_rio/data_science/svd/Main.java
+++ SVD/src/br/puc_rio/data_science/svd/Main.java
@@ -9,28 +9,52 @@ import javax.imageio.ImageIO;
 import Jama.SingularValueDecomposition;
 import Jama.Matrix;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		/* Pega uma imagem qualquer e transforma em uma matriz de pixels*/
+		
+	/* Pega uma imagem qualquer e transforma em uma matriz de pixels*/
 	    ImageToMatrix imageToMatrix = new ImageToMatrix();
 	   	BufferedImage image = null;
-		File arq = new File("images/image.jpg");
+		File arq = new File("images/PB.jpg");
 		try {
 			image = ImageIO.read(arq);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		Matrix A = new Matrix(imageToMatrix.imageToMatrix(image));// matriz gerada
+		System.out.println("linha="+A.getRowDimension()+" coluna ="+ A.getColumnDimension());
 		
-		/* DecomposiÃ§Ã£o da matriz da imagem nas suas componentes do SVD*/		
+	/* Decomposição da matriz da imagem nas suas componentes do SVD*/		
 		 SingularValueDecomposition SVD = A.svd();
 		 Matrix U = SVD.getU();
 		 Matrix S = SVD.getS();
 		 Matrix V = SVD.getV();
-		imageToMatrix.printMatrix(U.getArray(),image.getWidth(), image.getWidth());
-		imageToMatrix.printMatrix(S.getArray(),image.getHeight(), image.getHeight());
-		imageToMatrix.printMatrix(V.getArray(),image.getHeight(), image.getHeight());
+		 System.out.println("linha="+U.getRowDimension()+" coluna ="+ U.getColumnDimension());
+		 System.out.println("linha="+S.getRowDimension()+" coluna ="+ S.getColumnDimension());
+		 
+	/* porcentagens das matrizes U, V e S*/		
+		 Matrix U1= U.getMatrix(0, U.getRowDimension()-1, 0, (int)(U.getColumnDimension()*0.5));				
+		 Matrix S1= S.getMatrix(0, (int)(S.getRowDimension()*0.5), 0, (int)(S.getColumnDimension()*0.5));			
+		 Matrix V1= V.getMatrix(0, (int)(V.getRowDimension()*0.5), 0, V.getColumnDimension()-1);	
+		 
+		 System.out.println("linha="+U1.getRowDimension()+" coluna ="+ U1.getColumnDimension());
+		 System.out.println("linha="+S1.getRowDimension()+" coluna ="+ S1.getColumnDimension());
+		 
+		 Matrix US = U1.times(S1);
+		 Matrix A1 = US.times(V1);
+		 System.out.println("linha="+A1.getRowDimension()+" coluna ="+ A1.getColumnDimension());
+		 int [] v = imageToMatrix.matrixToVector(A1.getArray(), A1.getRowDimension(), A1.getColumnDimension());
+		BufferedImage image1 = imageToMatrix.getImage(A1.getArray(), A1.getRowDimension(), A1.getColumnDimension()) ;
+		try {
+			ImageIO.write(image1, "JPG", new File("images/gato54.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
    } 
 }
